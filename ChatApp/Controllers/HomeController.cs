9626ba3a93e7c5fd1f8f7f2066e9baef864a7268
OluwaseunSoft <oluwaseunazure@yahoo.com>
2020-01-15 14:25:51 +0100

    new file for chat app

---
+++ ChatApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+            public async Task<ActionResult> Pushermessage(string message)
+        {
+            var options = new PusherOptions();
+            options.Cluster = "mt1";
+            var pusher = new Pusher("932166", "e19424c57e2df62ff676", "3ebdf5f715c1973b537a", options);
+            ITriggerResult result = await pusher.TriggerAsync("my_channel", "my_event", new { message = message, name = "Anonymous" });
+
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+    }
+}
